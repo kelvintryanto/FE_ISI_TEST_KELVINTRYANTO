3321616f214530963577495ef3d83cd03f158f79
kelvintryanto <kelvin.tryanto@gmail.com>
2025-04-19 13:36:47 +0700

    fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ echo "Waiting for database to be ready..."
 until nc -z database 5432; do
   sleep 1
 done
-echo "Database is ready!✅"
+echo "Database is ready! ✅"
 
 # Jalankan migrate dev
 npx prisma migrate dev --name init
